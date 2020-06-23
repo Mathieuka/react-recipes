ca091dfe936eb3f1a29cc1f8cb95772e57c2c521
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-06-23 17:05:24 +0200

    populate graphql schema

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-require('dotenv').config({ path: 'variables.env' });
-const Recipe = require('./models/Recipe')
-const User = require('./models/User')
+const bodyParser = require('body-parser');
 
+require('dotenv').config({ path: 'variables.env' });
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// Bring in Graphql-Express middleware
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
 
 // Connects to database
 mongoose
-.connect(process.env.MONGO_URI)
-.then(() => console.log('db connected'))
-.catch(err => console.log('Error :: ', err))
-
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('db connected'))
+  .catch((err) => console.log('Error :: ', err));
 
 // Initializes app
 const app = express();
+
+// Create GraphiQl application
+app.use('./graphiql', graphiqlExpress({ endpointURL: '/graphql'}));
+
+// Connect schemas with GraphQl
+app.use('/graphql', graphqlExpress({
+    schema,
+    context: {
+        Recipe,
+        User
+    }
+}))
 
 const PORT = process.env.PORT || 4444;
 
