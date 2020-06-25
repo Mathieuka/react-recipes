5abf4a9b1895c23433d0e358fa661f99ca3e562d
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-06-25 17:14:17 +0200

    User password encoded with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const bcrypt = require('bcrypt');
+
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -18,10 +20,28 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-  favorites:  {
-      type: [Schema.Types.ObjectId],
-      ref: 'Recipe' 
+  favorites: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Recipe',
   },
+});
+
+// pre() function is triggered before element is saved in database
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
